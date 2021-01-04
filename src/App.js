3d47c78481c0bafe2b46f4ed90086303e4f5922f
Leonardo M. Primieri <leo.primieri@gmail.com>
2021-01-04 17:58:27 -0300

    adding seo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import SideBar from './components/SideBar/SideBar';
 import About from './screens/About/About';
 import Certificates from './screens/Certificates/Certificates';
@@ -17,22 +18,60 @@ function App() {
   const [linkClicked, setLinkClicked] = useState(false);
   const [mobile, setMobile] = useState(false);
   return (
-    <TransitionScreenContext.Provider value={{ linkClicked, setLinkClicked }}>
-      <MenuMobileContext.Provider value={{ setMobile }}>
-        <BrowserRouter>
-          <TransitionScreen transition={linkClicked} />
-          <SideBar mobile={mobile} />
-          <MenuButton setMobile={setMobile} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/github" component={GitHub} />
-            <Route path="/certificates" component={Certificates} />
-          </Switch>
-        </BrowserRouter>
-      </MenuMobileContext.Provider>
-    </TransitionScreenContext.Provider>
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Leonardo Merlo Primieri | Programador Front-End"
+        />
+
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta property="og:site_name" content="Leonardo Merlo Primieri" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta
+          property="og:url"
+          content="https://leonardomprimieri.vercel.app"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Leonardo Merlo Primieri | Programador Front-End"
+        />
+        <meta
+          property="og:description"
+          content="Olá, trabalho com desenvolvimento web há 2 anos e estou aqui para fazer seu site."
+        />
+
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+        <meta property="og:image:alt" content="Leonardo Merlo Primieri" />
+        <link rel="canonical" href="https://leonardomprimieri.vercel.app" />
+
+        <meta
+          name="keywords"
+          content="criar site, website, sistema, programador, programação, front-end, frontend, guanambi, bahia, brasil, site minimalista, quero criar site, quero criar meu site, negócio online, javascript, design, sistema web"
+        />
+        <meta name="author" content="Leonardo Merlo Primieri" />
+        <meta name="copyright" content="© Leonardo Merlo Primieri" />
+      </Helmet>
+      <TransitionScreenContext.Provider value={{ linkClicked, setLinkClicked }}>
+        <MenuMobileContext.Provider value={{ setMobile }}>
+          <BrowserRouter>
+            <TransitionScreen transition={linkClicked} />
+            <SideBar mobile={mobile} />
+            <MenuButton setMobile={setMobile} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/github" component={GitHub} />
+              <Route path="/certificates" component={Certificates} />
+            </Switch>
+          </BrowserRouter>
+        </MenuMobileContext.Provider>
+      </TransitionScreenContext.Provider>
+    </>
   );
 }
 

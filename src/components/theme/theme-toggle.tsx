363bb7c230af6932465ae5522c03ecac269fcdd1
Leonardo M. Primieri <leonardo.mprimieri@gmail.com>
2025-02-26 11:45:44 -0300

    feat: add header and dock nav

---
+++ src/components/theme/theme-toggle.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button } from "../ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="ghost"
+      type="button"
+      size="icon"
+      className="px-2"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
+      <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+    </Button>
+  );
+}
